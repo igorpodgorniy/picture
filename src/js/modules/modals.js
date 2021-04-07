2d5dad4c5a239674bf842ff8d0f5a1e18269ba81
Igor Podgorniy <ipod-92@mail.ru>
2021-04-07 20:05:42 +0300

    Добавил открытие модального окна по скроллу до конца страницы.

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,5 +1,6 @@
 const modals = () => {
-    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+    let btnPressed;
+    function bindModal(triggerSelector, modalSelector, closeSelector, destroy = false) {
         const trigger = document.querySelectorAll(triggerSelector);
         const modal = document.querySelector(modalSelector);
         const close = document.querySelector(closeSelector);
@@ -12,8 +13,15 @@ const modals = () => {
                     e.preventDefault();
                 }
 
+                btnPressed = true;
+
+                if (destroy) {
+                    item.remove();
+                }
+
                 windows.forEach(item => {
                     item.style.display = 'none';
+                    item.classList.add('animated', 'fadeIn');
                 });
     
                 modal.style.display = "block";
@@ -33,7 +41,7 @@ const modals = () => {
         });
 
         modal.addEventListener('click', e => {
-            if (e.target === modal && closeClickOverlay) {
+            if (e.target === modal) {
                 windows.forEach(item => {
                     item.style.display = 'none';
                 });
@@ -56,8 +64,10 @@ const modals = () => {
             });
 
             if (!display) {
+                const scroll = calcScroll();
                 document.querySelector(selector).style.display = "block";
                 document.body.style.overflow = 'hidden';
+                document.body.style.marginRight = `${scroll}px`;
             }
         }, time);
     }
@@ -78,9 +88,20 @@ const modals = () => {
         return scrollWidth;
     }
 
+    function openByScroll(selector) {
+        window.addEventListener('scroll', () => {
+            const scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
+            if (!btnPressed && (window.pageYOffset + document.documentElement.clientHeight) >= scrollHeight) {
+                document.querySelector(selector).click();
+            }
+        });
+    }
+
     bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
     bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
-    showModalByTime('.popup-consultation', 5000);
+    bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
+    openByScroll('.fixed-gift');
+    showModalByTime('.popup-consultation', 60000);
 };
 
 export default modals;
